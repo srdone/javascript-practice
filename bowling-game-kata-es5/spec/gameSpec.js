98fe04e82145663a5c49e0da0348f0d6f0959f41
Stephen Done <srdone@gmail.com>
2015-05-27 08:33:08 -0600

    refactored tests

--- bowling-game-kata-es5/spec/gameSpec.js
+++ bowling-game-kata-es5/spec/gameSpec.js
@@ -1,10 +1,17 @@
 describe('Bowling Game', function () {
 
+  var game;
+
+  beforeEach(function () {
+    game = new Game();
+  });
+
   it('should exist', function () {
-
-    var game = new Game();
-
     expect(game).toBeDefined();
+  });
+
+  it('should have a prototype of Game', function () {
+    expect(game instanceof Game).toBe(true);
   });
 
 });
